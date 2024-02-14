941fc03d47fa80aa1ab8bc1ccd9b8b3943cc00d3
N <southing602@gmail.com>
2024-02-14 16:49:29 +0900

    스타일 수정

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -7,30 +7,40 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        TextButton(
-          child: const Text('Increment Counter'),
-          onPressed: () => {
-            Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) =>
-                      const IncrementCounter(title: 'Increment Counter Demo'),
-                ))
-          },
-        ),
-        TextButton(
-          child: const Text('Layout Demo'),
-          onPressed: () => {
-            Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => const LayoutDemo(),
-                ))
-          },
-        ),
-      ],
+    return Padding(
+      padding: const EdgeInsets.all(20),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          TextButton(
+            style: ButtonStyle(
+              backgroundColor: MaterialStateProperty.all(Colors.grey),
+            ),
+            child: const Text('Increment Counter'),
+            onPressed: () => {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) =>
+                        const IncrementCounter(title: 'Increment Counter Demo'),
+                  ))
+            },
+          ),
+          TextButton(
+            style: ButtonStyle(
+              backgroundColor: MaterialStateProperty.all(Colors.grey),
+            ),
+            child: const Text('Layout Demo'),
+            onPressed: () => {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const LayoutDemo(),
+                  ))
+            },
+          ),
+        ],
+      ),
     );
   }
 }
